4e529912fe139500fa3d3a729a04ed1b9fb11cb1
jamesj995 <mrjam7@gmail.com>
2021-10-07 20:05:59 -0500

    added style to login page, added auth to all pages

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,6 @@ import Home from './pages/Home';
 import Detail from './pages/Detail';
 import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
 import MainNav from './components/MainNav';
 import { StoreProvider } from './utils/GlobalState';
 import Success from './pages/Success';
@@ -50,7 +49,6 @@ function App() {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path="/login" component={Login} />
-              <Route exact path="/signup" component={Signup} />
               <Route exact path="/success" component={Success} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
